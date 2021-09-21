fdb89538b56af6e96a4dc89e7c368897a9d7cafc
Quechus <quechus-96@hotmail.com>
2021-09-20 20:45:41 -0500

    excercise with astesisks

---
+++ src/excercises/squareAsterisks.java
@@ -0,0 +1,37 @@
+package excercises;
+
+import java.util.Scanner;
+
+public class squareAsterisks {
+	public static void main (String arg[]){
+		String entradaTeclado = "";
+
+        Scanner entradaEscaner = new Scanner (System.in); 
+        System.out.println("Ingrese el tamaño: ");
+        entradaTeclado = entradaEscaner.nextLine(); 
+        
+        try {
+        	int numAsteriscos = Integer.parseInt(entradaTeclado);
+        	for (int i = 0; i < numAsteriscos; i++) {
+    			if( i != 0 ) {
+    				System.out.println();
+    			}
+    			for (int j = 0; j < numAsteriscos; j++) {
+    				if( i != 0 && i != numAsteriscos - 1) {
+    					if(j > 0 && j < numAsteriscos - 1) {
+    						System.out.print(' ');
+    					} else {
+    						System.out.print('*');
+    					}
+    				} else {
+    					System.out.print('*');
+    				}	
+    			}
+    		}			
+         
+		} catch (Exception e) {
+			e.printStackTrace();  
+		}
+        
+	}	
+}
